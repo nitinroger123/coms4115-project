c3634d6a66ab98ec7f4b58fc5cada04bc2b237a4
nitinroger123 <nitinroger123@ec6ecee3-4e1f-15a6-26bb-b32b9f6144cc>
2011-02-21 20:14:13 +0000

    Adding some graph methods. Added some methods to Node. I feel it will be useful, we need to discuss this.

---
+++ trunk/src/exceptions/GraphNotLinearException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class GraphNotLinearException extends Exception {
+	private static final long serialVersionUID = 1565500742380746245L;
+	public GraphNotLinearException(String message){
+		super(message);
+	}
+
+}
--- trunk/src/interpreter/types/Node.java
+++ trunk/src/interpreter/types/Node.java
@@ -1,5 +1,42 @@
 package interpreter.types;
 
+import java.util.ArrayList;
+
 public class Node implements Type {
 	private Type value;
+	private ArrayList<Node> adjacentNodes;
+	
+	/**
+	 * Constructor to create a new node with a set value and set its adj nodes
+	 * @param value
+	 * @param adjacentNodes
+	 */
+	public Node(Type value, ArrayList<Node> adjacentNodes){
+		this.value=value;
+		this.adjacentNodes=adjacentNodes;
+	}
+	/**
+	 * returns the nodes adjacent to this particular node
+	 * @return
+	 */
+	public ArrayList<Node> getAdjacent(){
+		return adjacentNodes;
+	}
+	
+	public void addAdjacentNode(Node other){
+		this.adjacentNodes.add(other);
+	}
+	
+	public void addAdjacentNodes(ArrayList<Node> otherAdjacentNodes){
+		this.adjacentNodes.addAll(otherAdjacentNodes);
+	}
+
+	public void setValue(Type value) {
+		this.value = value;
+	}
+
+	public Type getValue() {
+		return value;
+	}
+	
 }
