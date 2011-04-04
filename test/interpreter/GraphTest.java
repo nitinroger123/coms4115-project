560b17da6a7212970f25625febde4eb159070118
nitinroger123 <nitinroger123@ec6ecee3-4e1f-15a6-26bb-b32b9f6144cc>
2011-04-04 21:11:48 +0000

    Checking in some tests

--- test/interpreter/GraphTest.java
+++ test/interpreter/GraphTest.java
@@ -1,5 +1,39 @@
 package interpreter;
 
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import interpreter.types.*;
+import interpreter.types.Number;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class GraphTest {
+	private Graph graph;
+	@Before
+	public void setUp() throws Exception {
+		graph = new Graph();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		graph = null;
+	}
+	
+	@Test
+	public void testNothing(){
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testAddNode(){
+		Number number = new Number(1.0);
+		Number number1 = new Number(2.0);
+		graph.addNode(new Node(number,null));
+		graph.addNode(new Node(number1,null));
+		assertEquals(2,graph.getNumberOfNodes());
+	}
 
 }
