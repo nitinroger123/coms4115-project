8dba0e248881f1e8b5311f7a458979248ca57f6c
liuyufei@gmail.com <liuyufei@gmail.com@ec6ecee3-4e1f-15a6-26bb-b32b9f6144cc>
2011-04-21 23:46:03 +0000

    changed file structure

---
+++ trunk/src/run.pl
@@ -0,0 +1,13 @@
+my $opt = $ARGV[0];
+
+if ($opt eq "clean") {
+    `rm parser/*.java`;
+    `rm parser/*.class`;
+    print "All parser-related java files and class files have been removes\n";
+    exit;
+}
+
+print `java -jar parser/resources/jacc.jar parser/GPL.jacc`;
+print `javac parser/GPLParser.java`;
+print `cat parser/test.gpl | java parser.Main`;
+
