a13c0e192f0d79fbcc9d8e443c82ead1e9cd2dd0
nitinroger123 <nitinroger123@ec6ecee3-4e1f-15a6-26bb-b32b9f6144cc>
2011-05-10 07:06:49 +0000

    Preprocessor updates. Now gets args and fixed a small bug

--- trunk/src/helper/FunctionDef.java
+++ trunk/src/helper/FunctionDef.java
@@ -10,6 +10,7 @@ public class FunctionDef {
 	public String name;
 	public String code;
 	public ArrayList<String> paramsType;
+	public ArrayList<String> args;
 	public FunctionDef(String name , String code , ArrayList<String> params){
 		this.name = name;
 		this.code = code;
